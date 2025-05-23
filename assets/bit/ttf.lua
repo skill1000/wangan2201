613879f0e14bdcc12d3099f62b26e1d45b8b3ae4
skill1000 <107780297+skill1000@users.noreply.github.com>
2025-05-23 08:43:01 +0800

    Delete assets directory

--- assets/bit/ttf.lua
+++
@@ -1,5 +0,0 @@
-import "android.graphics.Typeface"--导入包(必须)
-import "java.io.File"--导入包(必须)
-function ttf(hong,sk)--创建函数
-  hong.setTypeface(Typeface.createFromFile(File(activity.getLuaDir(sk))))
-end
